ce0bddb8943a659ccfb1de90fb7912b2fdd6ae4d
MagicBookPro <1240124885@qq.com>
2022-03-19 23:56:27 +0800

    'v2.2'

--- pages/data_analysis.py
+++ pages/data_analysis.py
@@ -4,7 +4,7 @@ import os
 import streamlit as st
 import geemap
 
-# # 设置环境变量
+# 设置环境变量
 os.environ["EARTHENGINE_TOKEN"] = st.secrets["EARTHENGINE_TOKEN"]
 
 # 定义全局变量（注意：当前路径是 app.py 所在文件夹的路径）
@@ -20,9 +20,9 @@ def app():
     m = vis_geemap()
     m.to_streamlit()
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def vis_geemap():
-    m = geemap.Map()
+    m = geemap.Map(center=[52.479215075, -1.9041145545], zoom=14)
     m.add_geojson(os.path.join(data_temp, 'building_footprints.geojson'), layer_name='building footprints')
     m.add_geojson(os.path.join(data_temp, 'pois.geojson'), layer_name='pois')
     m.add_geojson(os.path.join(data_temp, 'parking.json'), layer_name='parking')
