cfe3e74898835d1c334ec0e475b4c07b72d399a6
MagicBookPro <1240124885@qq.com>
2022-04-02 01:11:10 +0800

    'a'

--- pages/model_building.py
+++ pages/model_building.py
@@ -115,30 +115,27 @@ def training(col, train_dataset, train_batch_dataset, test_batch_dataset, epochs
 def evaluate():
     import sys
     import ssl
-    import time
     from pyngrok import ngrok
     import streamlit.components.v1 as components
     ssl._create_default_https_context = ssl._create_unverified_context
     
     col1, col2 = st.columns(2)
+    if col2.button('清空缓存', help='若页面报错, 清空缓存后重新运行TensorBoard!'):
+        if 'public_url' in st.session_state:
+            del st.session_state['public_url']
     if col1.button('TensorBoard', help='若无法显示, 尝试重新运行TensorBoard'):
         if 'public_url' not in st.session_state:
-            # 没有缓存，则启动并打开端口；有缓存直接打开端口。
             if sys.platform.startswith('win'):
                 os.system('start tensorboard --logdir ./data/output/logs/fit/ --port 6006')  # start 开启新进程
             elif sys.platform.startswith('linux'):
                 os.system(f'ngrok authtoken {st.secrets["NGROK_TOKEN"]}')
                 os.system('tensorboard --logdir ./data/output/logs/fit/ --port 6006 &')  # & 开启新进程
-            time.sleep(3)
-            # 根据端口生成公有网址
+            for i in ngrok.get_tunnels():
+                ngrok.disconnect(i.public_url)  # 在开启管道前，先清除所有存在的管道
             http_tunnel = ngrok.connect(addr='6006', proto='http')
             st.session_state.public_url = http_tunnel.public_url.replace('http:', 'https:')
         st.write('访问网页: ', st.session_state.public_url)
         components.iframe(st.session_state.public_url, height=600, scrolling=True)
-    if col2.button('清空缓存', help='若页面崩溃, 清空缓存后重新运行'):
-        del st.session_state.public_url
-        for i in ngrok.get_tunnels():
-            ngrok.disconnect(i.public_url)
 
 def prediction(col, train_dataset, train_labels, test_dataset, test_labels):
     if not os.path.exists(os.path.join('./data/output/models/', col)):
