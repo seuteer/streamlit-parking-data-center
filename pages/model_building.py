7f831e1c4c1401f4c203cc6fdecd6fc1db5a39d6
MagicBookPro <1240124885@qq.com>
2022-04-01 20:29:10 +0800

    'bug'

--- pages/model_building.py
+++ pages/model_building.py
@@ -133,7 +133,7 @@ def evaluate():
                 os.system('tensorboard --logdir ./data/output/logs/fit/ --port 6006 &')  # & 开启新进程
             # 根据端口生成公有网址
             http_tunnel = ngrok.connect(addr='6006', proto='http')
-            st.session_state.public_url = http_tunnel.public_url.replace('http', 'https')
+            st.session_state.public_url = http_tunnel.public_url.replace('http:', 'https:')
         st.write('访问网页: ', st.session_state.public_url)
         components.iframe(st.session_state.public_url, height=600, scrolling=True)
         if st.button('关闭TensorBoard'):
